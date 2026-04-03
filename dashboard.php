f36b71888e0a05a11f47ba79957568c2c62dec26
emmanuel louise arceo <emmanuellouisearceo@gmail.com>
2026-04-03 17:04:41 +0800

    commit 29/03/26

--- dashboard.php
+++ dashboard.php
@@ -116,8 +116,7 @@ $est_combined_date_display = null;
 $est5_raw = null;
 $est6_raw = null;
 
-// helper – add $days working days to a DateTime instance, skipping
-// Sunday and optionally Saturday.
+
 function add_working_days(DateTime $dt, int $days, bool $includeSat = true) {
     while ($days > 0) {
         $dt->modify('+1 day');
